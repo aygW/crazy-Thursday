5a3d419eb8b3e192cea16744365d925b023cc986
your name <your email>
2025-03-21 08:59:50 +0800

    main

--- py.D.A/homework/week4/jianda/text_precon.py
+++ py.D.A/homework/week4/jianda/text_precon.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import os
 
 df=pd.read_csv("D:/week4.csv")
 t_accuracy=str(input())
@@ -8,6 +9,10 @@ shop_ID=list(str(ID) for ID in set(df['shopID']))
 ID_dict={ID:{} for ID in shop_ID}
 com_dict={ID:set() for ID in shop_ID}
 
+def ensure_directory_exists(file_path):
+    directory=os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 def text_precon_period(t_module):
     def inner(string):
         ind=df[df['comment_time']==string].index[0]
@@ -21,18 +26,17 @@ def text_precon_period(t_module):
         else:
             ID_dict[ID][spltime].append(comment)
     return inner
-precon=text_precon_period(t_accuracy)
+precon=text_precon_period(t_accuracy) 
 df['comment_time'].apply(precon)
 
-def text_precon_alloc(c_dict):
+def text_precon_alloc(c_dict,t_module):
     for key in c_dict:
         for keyy in c_dict[key]:
-            file_name='D:/git code collections/crazy-Thursday/crazy-Thursday-1/py.D.A/week4/jianda/Q1/'+key+'/'+keyy+'.txt'
-            f=open(file_name,'w',encoding='utf-8')
-            #with open(file_name,'w') as f:
-            for com in c_dict[key][keyy]:
-                f.write(com+'\n')
-            f.close()
+            file_name='D:/git code collections/crazy-Thursday/py.D.A/homework/week4/jianda/Q1/'+t_module+'ly-seperated/'+key+'/'+keyy+'.txt'
+            ensure_directory_exists(file_name)
+            with open(file_name,'w',encoding='utf-8') as f:
+                for com in c_dict[key][keyy]:
+                    f.write(str(com)+'\n')
             com_dict[key].update(file_name)
     return
-text_precon_alloc(ID_dict)
+text_precon_alloc(ID_dict,t_accuracy)
